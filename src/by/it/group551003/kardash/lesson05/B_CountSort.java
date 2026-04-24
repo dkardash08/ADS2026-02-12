2404dd8c60b3b7c6fae4eda9c3e45563403288cf
dkardash08 <monafrankina@gmail.com>
2026-04-24 11:23:10 +0300

    lesson 05

--- src/by/it/group551003/kardash/lesson05/B_CountSort.java
+++ src/by/it/group551003/kardash/lesson05/B_CountSort.java
@@ -3,7 +3,15 @@ package by.it.group551003.kardash.lesson05;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
+/*
+Первая строка содержит число 1<=n<=10000, вторая - n натуральных чисел, не превышающих 10.
+Выведите упорядоченную по неубыванию последовательность этих чисел.
 
+При сортировке реализуйте метод со сложностью O(n)
+
+Пример: https://karussell.wordpress.com/2010/03/01/fast-integer-sorting-algorithm-on/
+Вольный перевод: http://programador.ru/sorting-positive-int-linear-time/
+*/
 public class B_CountSort {
 
     public static void main(String[] args) throws FileNotFoundException {
